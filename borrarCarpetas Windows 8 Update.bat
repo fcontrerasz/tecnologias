2e347d2f65657914c7703a1ed7aa37f4880d9e91
fcontrerasz <fcontrerasz@gmail.com>
2014-04-28 11:31:33 -0400

    version 1

---
+++ borrarCarpetas Windows 8 Update.bat
@@ -0,0 +1,58 @@
+md borrar
+
+robocopy /e /purge /b borrar 08943b84dd18fcbcac
+robocopy /e /purge /b borrar 0b1c2214c8d7b8664bd441b0
+robocopy /e /purge /b borrar 19b371546b86d48b0b06
+robocopy /e /purge /b borrar 203c447a3b7ba49d70
+robocopy /e /purge /b borrar 226184545875c068fd6899
+robocopy /e /purge /b borrar 2708b8dfcbeb4dfac123aabc992e1f9d
+robocopy /e /purge /b borrar 3eba3a770a69e1891088f7ef38b3ca
+robocopy /e /purge /b borrar 5397b38a09d63050a49104ed
+robocopy /e /purge /b borrar baa1bce3d678a795213e79
+robocopy /e /purge /b borrar bf9dadd14a172f936f0d9dadf259872f
+robocopy /e /purge /b borrar e3bc5add7d5329f447fd2b38
+robocopy /e /purge /b borrar ec6bc7a9fd9fb9a359f271a3fd
+robocopy /e /purge /b borrar fa2700cb0366fabacf12de
+
+
+takeown /F 08943b84dd18fcbcac
+takeown /F 0b1c2214c8d7b8664bd441b0
+takeown /F 19b371546b86d48b0b06
+takeown /F 203c447a3b7ba49d70
+takeown /F 226184545875c068fd6899
+takeown /F 2708b8dfcbeb4dfac123aabc992e1f9d
+takeown /F 3eba3a770a69e1891088f7ef38b3ca
+takeown /F 5397b38a09d63050a49104ed
+takeown /F baa1bce3d678a795213e79
+takeown /F bf9dadd14a172f936f0d9dadf259872f
+takeown /F e3bc5add7d5329f447fd2b38
+takeown /F ec6bc7a9fd9fb9a359f271a3fd
+takeown /F fa2700cb0366fabacf12de
+
+icacls 08943b84dd18fcbcac /grant Administradores:F
+icacls 0b1c2214c8d7b8664bd441b0 /grant Administradores:F
+icacls 19b371546b86d48b0b06 /grant Administradores:F
+icacls 203c447a3b7ba49d70 /grant Administradores:F
+icacls 226184545875c068fd6899 /grant Administradores:F
+icacls 2708b8dfcbeb4dfac123aabc992e1f9d /grant Administradores:F
+icacls 3eba3a770a69e1891088f7ef38b3ca /grant Administradores:F
+icacls 5397b38a09d63050a49104ed /grant Administradores:F
+icacls baa1bce3d678a795213e79 /grant Administradores:F
+icacls bf9dadd14a172f936f0d9dadf259872f /grant Administradores:F
+icacls e3bc5add7d5329f447fd2b38 /grant Administradores:F
+icacls ec6bc7a9fd9fb9a359f271a3fd /grant Administradores:F
+icacls fa2700cb0366fabacf12de /grant Administradores:F
+
+rd 08943b84dd18fcbcac
+rd 0b1c2214c8d7b8664bd441b0
+rd 19b371546b86d48b0b06
+rd 203c447a3b7ba49d70
+rd 226184545875c068fd6899
+rd 2708b8dfcbeb4dfac123aabc992e1f9d
+rd 3eba3a770a69e1891088f7ef38b3ca
+rd 5397b38a09d63050a49104ed
+rd baa1bce3d678a795213e79
+rd bf9dadd14a172f936f0d9dadf259872f
+rd e3bc5add7d5329f447fd2b38
+rd ec6bc7a9fd9fb9a359f271a3fd
+rd fa2700cb0366fabacf12de
---
+++ getIP.bat
@@ -0,0 +1,2 @@
+@echo off
+ping google.cl -t
